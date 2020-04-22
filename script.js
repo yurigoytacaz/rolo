defbb0c4a01a411019b1c6978bad73b46ec03396
Yuri Goytacaz <yurigoytacaz@gmail.com>
2020-04-22 19:44:29 -0300

    adiciona grids nas number keys

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // @TODO add controls to screen
 // @TODO load new layouts
 
-const gridSolution = [
+let gridSolution = [
   [1,1,1,1,1,1],
   [1,1,1,1,1,1],
   [1,1,2,2,1,1],
@@ -217,10 +217,11 @@ const KEY_UP = 38;
 const KEY_RIGHT = 39;
 const KEY_DOWN = 40;
 const KEY_SPACE = 32;
+const KEY_NUMZERO = 48
 
 document.addEventListener('keydown', function (e) {
   var code = e.keyCode ? e.keyCode : e.which;
-  // console.log(code);
+  console.log(code);
 
   switch (code) {
     case KEY_SPACE:
@@ -237,6 +238,76 @@ document.addEventListener('keydown', function (e) {
           shuffleTable();
           colorTable(gridPuzzle, tablePuzzle);
         }
+      }
+    break;
+
+    case KEY_NUMZERO+1:
+      if (window.confirm("Do you really want to RELOAD it?")) {
+        gridSolution = grid1
+        start()
+      }
+    break;
+
+    case KEY_NUMZERO+2:
+      if (window.confirm("Do you really want to RELOAD it?")) {
+        gridSolution = grid2
+        start()
+      }
+    break;
+
+    case KEY_NUMZERO+3:
+      if (window.confirm("Do you really want to RELOAD it?")) {
+        gridSolution = grid3
+        start()
+      }
+    break;
+
+    case KEY_NUMZERO+4:
+      if (window.confirm("Do you really want to RELOAD it?")) {
+        gridSolution = grid4
+        start()
+      }
+    break;
+
+    case KEY_NUMZERO+5:
+      if (window.confirm("Do you really want to RELOAD it?")) {
+        gridSolution = grid5
+        start()
+      }
+    break;
+
+    case KEY_NUMZERO+6:
+      if (window.confirm("Do you really want to RELOAD it?")) {
+        gridSolution = grid6
+        start()
+      }
+    break;
+
+    case KEY_NUMZERO+7:
+      if (window.confirm("Do you really want to RELOAD it?")) {
+        gridSolution = grid7
+        start()
+      }
+    break;
+
+    case KEY_NUMZERO+8:
+      if (window.confirm("Do you really want to RELOAD it?")) {
+        gridSolution = grid8
+        start()
+      }
+    break;
+
+    case KEY_NUMZERO+9:
+      if (window.confirm("Do you really want to RELOAD it?")) {
+        gridSolution = grid9
+        start()
+      }
+    break;
+
+    case KEY_NUMZERO+0:
+      if (window.confirm("Do you really want to RELOAD it?")) {
+        gridSolution = grid0
+        start()
       }
     break;
 
@@ -295,12 +366,108 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-console.log("ROLLING >>>> ");
-colorTable(gridSolution, tableSolution);
-shuffleTable();
-colorTable(gridPuzzle, tablePuzzle);
-rowSelected = Math.min(0, rowSelected + 1);
-selectRow(tablePuzzle);
-colSelected = Math.max(0, colSelected - 1);
-selectCol(tablePuzzle);
-console.log("ROLLED <<<<");
+function start(){
+  console.log("ROLLING >>>> ");
+  colorTable(gridSolution, tableSolution);
+  shuffleTable();
+  colorTable(gridPuzzle, tablePuzzle);
+  rowSelected = Math.min(0, rowSelected + 1);
+  selectRow(tablePuzzle);
+  colSelected = Math.max(0, colSelected - 1);
+  selectCol(tablePuzzle);
+  console.log("ROLLED <<<<");
+}
+
+start();
+
+
+const grid1 = [
+  [1,1,1,1,1,1],
+  [1,1,1,1,1,1],
+  [1,1,2,2,1,1],
+  [1,1,2,2,1,1],
+  [1,1,1,1,1,1],
+  [1,1,1,1,1,1]
+];
+
+const grid2 = [
+  [1,1,1,1,1,1],
+  [1,2,2,2,2,1],
+  [1,2,1,1,2,1],
+  [1,2,1,1,2,1],
+  [1,2,2,2,2,1],
+  [1,1,1,1,1,1]
+];
+
+const grid3 = [
+  [6,6,6,6,6,6],
+  [6,6,6,6,6,6],
+  [0,0,6,6,0,0],
+  [0,0,6,6,0,0],
+  [6,6,6,6,6,6],
+  [6,6,6,6,6,6]
+];
+
+const grid4 = [
+  [1,1,4,4,1,1],
+  [1,1,4,4,1,1],
+  [4,4,4,4,4,4],
+  [4,4,4,4,4,4],
+  [1,1,4,4,1,1],
+  [1,1,4,4,1,1]
+];
+
+const grid5 = [
+  [0,0,0,0,0,0],
+  [0,0,0,0,0,0],
+  [1,1,1,1,1,1],
+  [1,1,1,1,1,1],
+  [2,2,1,1,2,2],
+  [2,2,1,1,2,2]
+];
+
+const grid6 = [
+  [4,4,4,4,4,4],
+  [5,5,5,5,5,5],
+  [0,0,0,0,0,0],
+  [5,5,5,5,5,5],
+  [3,3,3,3,3,3],
+  [5,5,5,5,5,5]
+];
+
+const grid7 = [
+  [1,1,1,0,0,0],
+  [1,1,1,0,0,0],
+  [1,1,1,0,0,0],
+  [3,3,3,4,4,4],
+  [3,3,3,4,4,4],
+  [3,3,3,4,4,4]
+];
+
+const grid8 = [
+  [1,1,3,2,2,3],
+  [1,1,3,2,2,3],
+  [1,1,3,2,2,3],
+  [1,1,3,2,2,3],
+  [1,1,3,2,2,3],
+  [1,1,3,2,2,3]
+];
+
+const grid9 = [
+  [5,5,4,4,1,1],
+  [2,5,5,4,4,1],
+  [2,2,5,5,4,4],
+  [2,2,2,5,5,4],
+  [2,2,2,2,5,4],
+  [2,2,2,2,5,4]
+];
+
+const grid0 = [
+  [2,0,3,2,1,4],
+  [1,2,2,1,1,5],
+  [0,1,1,1,1,1],
+  [1,4,4,5,1,2],
+  [0,2,0,2,4,0],
+  [4,4,4,3,3,5]
+];
+
